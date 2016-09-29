2ec1e3b2801ef7b3f830df3e226949c3b7f4ec6f
Andrew <andrew.vinocur@gmail.com>
2016-09-28 20:11:36 -0700

    fixes error from 5.6 and completes 5.7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
